57b22c588d988e1492dc61fb74fc3f13fe27e6fb
Nhan Le <nhan.le@altsourcesoftware.com>
2023-08-18 11:25:55 +0700

    Add eslint-typescript, sort imports, axios, pinia

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,25 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
 
-createApp(App).mount('#app')
+import { createPinia } from 'pinia';
+import Toast, {
+  POSITION,
+  PluginOptions as ToastOptions,
+} from 'vue-toastification';
+
+import './style.css';
+import 'vue-toastification/dist/index.css';
+
+import App from './App.vue';
+
+const pinia = createPinia();
+const app = createApp(App);
+
+const toastOptions: ToastOptions = {
+  position: POSITION.BOTTOM_RIGHT,
+  hideProgressBar: true,
+};
+
+app.use(pinia);
+app.use(Toast, toastOptions);
+
+app.mount('#app');
