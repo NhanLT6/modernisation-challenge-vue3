b9dd6249c02fb3d7de38bc35d4bb9106655d72fb
Nhan Le <nhan.le@altsourcesoftware.com>
2023-09-03 12:30:43 +0700

    Deploy app to GitHub pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/modernisation-challenge-vue3/' : '/',
   plugins: [vue(), eslintPlugin()],
   resolve: {
     alias: [
